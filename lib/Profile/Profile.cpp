d83f37e0d480f1fe0bd9e3acdb90312c2444b149
COMPOSERZZ <edsonreaper@outlook.es>
2026-07-12 23:32:50 -0600

    a

--- lib/Profile/Profile.cpp
+++ lib/Profile/Profile.cpp
@@ -12,28 +12,162 @@ void Profile::begin()
         }
     }
 
-    InteractionConfig instantKeyConfig;
-    instantKeyConfig.enableClick = false;
-    instantKeyConfig.enableDoubleClick = false;
-    instantKeyConfig.enableLongPress = false;
-    instantKeyConfig.enableHoldRepeat = false;
+    InteractionConfig pressOnly;
+    pressOnly.enableClick = false;
+    pressOnly.enableDoubleClick = false;
+    pressOnly.enableLongPress = false;
+    pressOnly.enableHoldRepeat = false;
 
-    // Botones 1, 2 y 3: teclas instantáneas.
-    interactionConfigs[0] = instantKeyConfig;
-    interactionConfigs[1] = instantKeyConfig;
-    interactionConfigs[2] = instantKeyConfig;
+    InteractionConfig clickOnly;
+    clickOnly.enableClick = true;
+    clickOnly.enableDoubleClick = false;
+    clickOnly.enableLongPress = false;
+    clickOnly.enableHoldRepeat = false;
 
+    InteractionConfig doubleClickOnly;
+    doubleClickOnly.enableClick = false;
+    doubleClickOnly.enableDoubleClick = true;
+    doubleClickOnly.enableLongPress = false;
+    doubleClickOnly.enableHoldRepeat = false;
+
+    InteractionConfig longPressOnly;
+    longPressOnly.enableClick = false;
+    longPressOnly.enableDoubleClick = false;
+    longPressOnly.enableLongPress = true;
+    longPressOnly.enableHoldRepeat = false;
+
+    InteractionConfig longPressWithRepeat;
+    longPressWithRepeat.enableClick = false;
+    longPressWithRepeat.enableDoubleClick = false;
+    longPressWithRepeat.enableLongPress = true;
+    longPressWithRepeat.enableHoldRepeat = true;
+
+    InteractionConfig clickAndDoubleClick;
+    clickAndDoubleClick.enableClick = true;
+    clickAndDoubleClick.enableDoubleClick = true;
+    clickAndDoubleClick.enableLongPress = false;
+    clickAndDoubleClick.enableHoldRepeat = false;
+
+    InteractionConfig clickAndLongPress;
+    clickAndLongPress.enableClick = true;
+    clickAndLongPress.enableDoubleClick = false;
+    clickAndLongPress.enableLongPress = true;
+    clickAndLongPress.enableHoldRepeat = false;
+
+    InteractionConfig slowDoubleClick = doubleClickOnly;
+    slowDoubleClick.doubleClickTime = 500;
+
+    InteractionConfig slowLongPress = longPressOnly;
+    slowLongPress.longPressTime = 1200;
+
+    InteractionConfig fastRepeat = longPressWithRepeat;
+    fastRepeat.repeatTime = 50;
+
+    // Configuración de interacción por botón.
+    interactionConfigs[0] = pressOnly;
+    interactionConfigs[1] = clickOnly;
+    interactionConfigs[2] = doubleClickOnly;
+    interactionConfigs[3] = longPressOnly;
+    interactionConfigs[4] = longPressWithRepeat;
+    interactionConfigs[5] = clickAndDoubleClick;
+    interactionConfigs[6] = pressOnly;
+    interactionConfigs[7] = clickAndLongPress;
+    interactionConfigs[8] = slowDoubleClick;
+    interactionConfigs[9] = slowLongPress;
+    interactionConfigs[10] = fastRepeat;
+    interactionConfigs[11] = pressOnly;
+    interactionConfigs[12] = clickOnly;
+
+    // BTN 1: PRESS.
     actions[0][eventToIndex(ButtonEvent::PRESS)].type =
         ActionType::KEYBOARD_KEY;
     actions[0][eventToIndex(ButtonEvent::PRESS)].data[0] = '1';
 
-    actions[1][eventToIndex(ButtonEvent::PRESS)].type =
+    // BTN 2: CLICK.
+    actions[1][eventToIndex(ButtonEvent::CLICK)].type =
         ActionType::KEYBOARD_KEY;
-    actions[1][eventToIndex(ButtonEvent::PRESS)].data[0] = '4';
+    actions[1][eventToIndex(ButtonEvent::CLICK)].data[0] = '2';
 
-    actions[2][eventToIndex(ButtonEvent::PRESS)].type =
+    // BTN 3: DOUBLE_CLICK.
+    actions[2][eventToIndex(ButtonEvent::DOUBLE_CLICK)].type =
         ActionType::KEYBOARD_KEY;
-    actions[2][eventToIndex(ButtonEvent::PRESS)].data[0] = '7';
+    actions[2][eventToIndex(ButtonEvent::DOUBLE_CLICK)].data[0] = '3';
+
+    // BTN 4: LONG_PRESS.
+    actions[3][eventToIndex(ButtonEvent::LONG_PRESS)].type =
+        ActionType::KEYBOARD_KEY;
+    actions[3][eventToIndex(ButtonEvent::LONG_PRESS)].data[0] = '4';
+
+    // BTN 5: LONG_PRESS y HOLD_REPEAT.
+    actions[4][eventToIndex(ButtonEvent::LONG_PRESS)].type =
+        ActionType::KEYBOARD_KEY;
+    actions[4][eventToIndex(ButtonEvent::LONG_PRESS)].data[0] = '5';
+
+    actions[4][eventToIndex(ButtonEvent::HOLD_REPEAT)].type =
+        ActionType::KEYBOARD_KEY;
+    actions[4][eventToIndex(ButtonEvent::HOLD_REPEAT)].data[0] = 'r';
+
+    // BTN 6: CLICK o DOUBLE_CLICK.
+    actions[5][eventToIndex(ButtonEvent::CLICK)].type =
+        ActionType::KEYBOARD_KEY;
+    actions[5][eventToIndex(ButtonEvent::CLICK)].data[0] = '6';
+
+    actions[5][eventToIndex(ButtonEvent::DOUBLE_CLICK)].type =
+        ActionType::KEYBOARD_KEY;
+    actions[5][eventToIndex(ButtonEvent::DOUBLE_CLICK)].data[0] = 'D';
+
+    // BTN 7: PRESS y RELEASE.
+    actions[6][eventToIndex(ButtonEvent::PRESS)].type =
+        ActionType::KEYBOARD_KEY;
+    actions[6][eventToIndex(ButtonEvent::PRESS)].data[0] = '7';
+
+    actions[6][eventToIndex(ButtonEvent::RELEASE)].type =
+        ActionType::KEYBOARD_KEY;
+    actions[6][eventToIndex(ButtonEvent::RELEASE)].data[0] = 'R';
+
+    // BTN 8: CLICK o LONG_PRESS.
+    actions[7][eventToIndex(ButtonEvent::CLICK)].type =
+        ActionType::KEYBOARD_KEY;
+    actions[7][eventToIndex(ButtonEvent::CLICK)].data[0] = '8';
+
+    actions[7][eventToIndex(ButtonEvent::LONG_PRESS)].type =
+        ActionType::KEYBOARD_KEY;
+    actions[7][eventToIndex(ButtonEvent::LONG_PRESS)].data[0] = 'L';
+
+    // BTN 9: DOUBLE_CLICK lento.
+    actions[8][eventToIndex(ButtonEvent::DOUBLE_CLICK)].type =
+        ActionType::KEYBOARD_KEY;
+    actions[8][eventToIndex(ButtonEvent::DOUBLE_CLICK)].data[0] = '9';
+
+    // BTN 10: LONG_PRESS lento.
+    actions[9][eventToIndex(ButtonEvent::LONG_PRESS)].type =
+        ActionType::KEYBOARD_KEY;
+    actions[9][eventToIndex(ButtonEvent::LONG_PRESS)].data[0] = '0';
+
+    // BTN 11: LONG_PRESS con repetición rápida.
+    actions[10][eventToIndex(ButtonEvent::LONG_PRESS)].type =
+        ActionType::KEYBOARD_KEY;
+    actions[10][eventToIndex(ButtonEvent::LONG_PRESS)].data[0] = 'H';
+
+    actions[10][eventToIndex(ButtonEvent::HOLD_REPEAT)].type =
+        ActionType::KEYBOARD_KEY;
+    actions[10][eventToIndex(ButtonEvent::HOLD_REPEAT)].data[0] = '+';
+
+    // BTN 12: PRESS y RELEASE.
+    actions[11][eventToIndex(ButtonEvent::PRESS)].type =
+        ActionType::KEYBOARD_KEY;
+    actions[11][eventToIndex(ButtonEvent::PRESS)].data[0] = 'A';
+
+    actions[11][eventToIndex(ButtonEvent::RELEASE)].type =
+        ActionType::KEYBOARD_KEY;
+    actions[11][eventToIndex(ButtonEvent::RELEASE)].data[0] = 'a';
+
+    // BTN 13: CLICK.
+    actions[12][eventToIndex(ButtonEvent::CLICK)].type =
+        ActionType::KEYBOARD_KEY;
+    actions[12][eventToIndex(ButtonEvent::CLICK)].data[0] = 'B';
+
+    // BTN 14 y BTN 15 quedan sin acción intencionalmente.
 }
 
 Action Profile::getAction(uint8_t buttonID, ButtonEvent event) const
